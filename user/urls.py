ad7a5f2977a0776269403500e50e54f563e5e79b
Sylph <ngmanhdung2003@gmail.com>
2025-01-14 01:32:31 +0700

    update

--- user/urls.py
+++ user/urls.py
@@ -4,5 +4,5 @@ from .views import RegisterView, LoginView, UserListView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('users/', UserListView.as_view(), name='user-list'),  # Đường dẫn liệt kê người dùng
+    path('get_users/', UserListView.as_view(), name='user-list'),  # Đường dẫn liệt kê người dùng
 ]
